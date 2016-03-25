a7164062395eb0ec492c2e3ed0783ca129a733e3
Alain Schlesser <alain.schlesser@gmail.com>
2016-03-25 07:05:22 +0100

    Add graceful error-handling to bootstrapping process.

--- includes/bootstrap.php
+++ includes/bootstrap.php
@@ -11,6 +11,7 @@
 
 use PHPComposter\PHPComposter\Paths;
 
+// Get the command-line arguments passed from the shell script.
 global $argv;
 $hook = $argv[1];
 $root = $argv[2];
@@ -20,10 +21,18 @@ if (file_exists($root . '/vendor/autoload.php')) {
     require_once $root . '/vendor/autoload.php';
 }
 
+// Read the configuration file.
 $config = include Paths::getPath('git_config');
+
+// Iterate over hook methods.
 if (array_key_exists($hook, $config)) {
+
     $array = $config[$hook];
+
+    // Sort by priority.
     ksort($array);
+
+    // Launch each method.
     foreach ($array as $methods) {
         foreach ($methods as $method) {
             $method($hook, $root);
