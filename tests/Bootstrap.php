2949235bae7ca1b4a2c1ee6633813ffbef9c3d71
Franz Josef Kaiser <wecodemore@gmail.com>
2016-04-07 19:49:28 +0200

    feat(vcs) Add new files to ignore list, see #4

---
+++ tests/Bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * This file is part of the "" package.
+ *
+ * © 2016 Franz Josef Kaiser
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
