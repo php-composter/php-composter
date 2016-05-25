250769775328bf94b4fc23d34f4ff43e24e1db08
Alain Schlesser <alain.schlesser@gmail.com>
2016-05-25 13:18:09 +0200

    Add `getPath()` unit test.

--- tests/src/Unit/PathsTest.php
+++ tests/src/Unit/PathsTest.php
@@ -10,9 +10,51 @@
 
 namespace PHPComposter\Tests\Unit;
 
+use PHPComposter\PHPComposter\Paths;
 use PHPComposter\Tests\TestCase;
 
 class PathsTest extends TestCase
 {
-	#public function test
+
+    /**
+     * Test whether getPath() returns matching strings.
+     *
+     * @since        0.3.0
+     *
+     * @dataProvider getPathDataProvider
+     *
+     * @param string $key            The key to fetch.
+     * @param string $expectedFormat A RegEx that defines the path that should be returned.
+     */
+    public function testGetPathReturnsMatchingStrings($key, $expectedFormat)
+    {
+        $path = Paths::getPath($key);
+        $this->assertInternalType('string', $path);
+        $this->assertNotEmpty($path);
+        $this->assertRegExp($expectedFormat, $path);
+    }
+
+    /**
+     * Provide test data to testGetPathReturnsMatchingStrings().
+     *
+     * @since 0.3.0
+     *
+     * @return array Array of string couples with test data.
+     */
+    public function getPathDataProvider()
+    {
+        return [
+            // $key, $expectedFormat
+            ['pwd', '|(.*)/$|'],
+            ['root_git', '|(.*)/.git/$|'],
+            ['root_hooks', '|(.*)/.git/hooks/$|'],
+            ['vendor_composter', '|(.*)/vendor/php-composter/php-composter/$|'],
+            ['git_composter', '|(.*)/.git/php-composter/$|'],
+            ['git_script', '|(.*)/php-composter$|'],
+            ['actions', '|(.*)/.git/php-composter/actions/$|'],
+            ['git_template', '|(.*)/vendor/php-composter/php-composter/includes/$|'],
+            ['root_template', '|(.*)/.git/php-composter/includes/$|'],
+            ['git_config', '|(.*)/.git/php-composter/config.php$|'],
+        ];
+    }
 }
